a053259876ed5d1c92050b7f027360888759a6ff
adinaavram878 <ralucaadinaavram@yahoo.com>
2025-08-28 07:11:05 +0100

    Update getCountryBorder.php

--- Project 1/php/getCountryBorder.php
+++ Project 1/php/getCountryBorder.php
@@ -1,1 +1,19 @@
+<?php
+header('Content-Type: application/json');
+
+$countryCode = strtolower($_POST['code'] ?? '');
+
+$geojson = file_get_contents('../data/countries.geojson');
+$data = json_decode($geojson, true);
+
+foreach ($data['features'] as $feature) {
+    if (strtolower($feature['properties']['ISO_A2']) === $countryCode) {
+        echo json_encode($feature);
+        exit;
+    }
+}
+
+http_response_code(404);
+echo json_encode(['error' => 'Country not found']);
+?>
 
