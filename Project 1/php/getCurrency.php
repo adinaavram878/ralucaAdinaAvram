50f56ba3b82857e8d3227f118bec55ed13188499
adinaavram878 <ralucaadinaavram@yahoo.com>
2025-09-19 09:33:21 +0100

    Update getCurrency.php

--- Project 1/php/getCurrency.php
+++ Project 1/php/getCurrency.php
@@ -1,6 +1,10 @@
-
 <?php
 header('Content-Type: application/json; charset=UTF-8');
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 
 $input = json_decode(file_get_contents('php://input'), true);
 $targetCurrency = $input['code'] ?? 'USD';
