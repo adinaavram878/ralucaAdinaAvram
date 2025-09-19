d84e2190503316ac6d23dbaf63956097061d556a
adinaavram878 <ralucaadinaavram@yahoo.com>
2025-09-19 09:27:24 +0100

    Update getCurrency.php

--- Project 1/php/getCurrency.php
+++ Project 1/php/getCurrency.php
@@ -1,11 +1,12 @@
+
 <?php
+header('Content-Type: application/json; charset=UTF-8');
 
-header('Content-Type: application/json');
+$input = json_decode(file_get_contents('php://input'), true);
+$targetCurrency = $input['code'] ?? 'USD';
 
-$currency = $_GET['currency'] ?? 'USD';
 $apiKey = '3c864c951e2a21a7fa71b6c2b2beb61a';
-
-$url = "http://data.fixer.io/api/latest?access_key=$apiKey&symbols=$currency";
+$url = "http://data.fixer.io/api/latest?access_key=$apiKey&symbols=USD,$targetCurrency";
 
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -14,12 +15,22 @@ curl_close($ch);
 
 $data = json_decode($response, true);
 
-if (!$data || !$data['success'] || !isset($data['rates'][$currency])) {
-    echo json_encode(['error' => 'Failed to retrieve exchange rate']);
+
+if (!$data || !isset($data['rates']['USD']) || !isset($data['rates'][$targetCurrency])) {
+    echo json_encode([
+        'error' => 'Invalid response from Fixer API',
+    ]);
     exit;
 }
 
+$eurToUsd = $data['rates']['USD'];
+$eurToTarget = $data['rates'][$targetCurrency];
+
+
+$usdToTarget = $eurToTarget / $eurToUsd;
+
 echo json_encode([
-    'currency' => $currency,
-    'rate' => $data['rates'][$currency]
+    'currency' => $targetCurrency,
+    'rate' => $usdToTarget
 ]);
+
